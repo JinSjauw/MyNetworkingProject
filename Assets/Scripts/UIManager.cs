24856935af89edec5569ccc367835721b07c14fe
JinSjauw <jinzhouliu4@gmail.com>
2023-01-13 20:01:47 +0100

    Initial commit

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    public GameObject startMenu;
+    public TextMeshProUGUI usernameField;
+    
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Debug.Log("Instance Already exist, destroying object");
+            Destroy(this);
+        }
+    }
+
+    public void ConnectToServer()
+    {
+        startMenu.SetActive(false);
+        usernameField.enabled = false;
+        Client.instance.ConnectToServer();
+    }
+}
